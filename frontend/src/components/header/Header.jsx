f1ef2e9bd64afa6c131567982c51b37f386f2e83
adinduokoro <107534645+adinduokoro@users.noreply.github.com>
2025-10-26 10:52:23 -0400

    Header
    The header structure is created.

--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from "react";
+import styles from "./Header.module.css";
+import moon from "../../assets/icons/moon.svg";
 
 const Header = () => {
   return (
-    <div>
-      This is the header
+    <div className={styles.header}>
+      <div className={styles.logo}>
+        <div className={`${styles.logoIconContainer} body-text`}>AI</div>
+        <div className={`${styles.headerTitle} header`}>Internal AI Assistant</div>
+      </div>
+      <button className={styles.themeButton}>
+        <img src={moon} alt="Activate dark mode" />
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
